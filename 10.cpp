b25b73bfc9cae1b15272d77a2f73dec1e25e1f6d
shuol <oliver.shu@oocl.com>
2019-09-03 15:01:40 +0800

    10

---
+++ 10.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2019-09-03 15:00:36 
+ * @Last Modified by: Oliver Seth
+ * @Last Modified time: 2019-09-03 15:01:09
+ */
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return regex_match(s,regex(p));
+    }
+};
