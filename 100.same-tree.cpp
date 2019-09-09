318c3106174f32c36a68325dc5ed7b5d72e1385d
shuol <oliver.shu@oocl.com>
2019-09-09 14:56:38 +0800

    100

---
+++ 100.same-tree.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2019-09-09 14:47:56 
+ * @Last Modified by: Oliver Seth
+ * @Last Modified time: 2019-09-09 14:56:01
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if ((!p) && (!q))
+            return true;
+        if ((!p) || (!q))
+            return false;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
