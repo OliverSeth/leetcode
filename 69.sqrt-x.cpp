f464670fc3053e1c1c05dbf0767fdbfb2e767055
OliverSeth <szl980731@126.com>
2019-09-14 11:39:04 +0800

    69

---
+++ 69.sqrt-x.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2019-09-14 11:36:54 
+ * @Last Modified by: Oliver Seth
+ * @Last Modified time: 2019-09-14 11:38:43
+ */
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        return sqrt(x);
+    }
+};
