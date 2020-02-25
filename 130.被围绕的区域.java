4de7c23cd2a4512d999be392624b5a7d837fc1ba
OliverSeth <szl980731@126.com>
2020-02-25 21:00:32 +0800

    130

---
+++ 130.被围绕的区域.java
@@ -0,0 +1,50 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2020-02-24 19:50:23 
+ * @Last Modified by: Oliver Seth
+ * @Last Modified time: 2020-02-24 20:21:10
+ */
+/*
+ * @lc app=leetcode.cn id=130 lang=java
+ *
+ * [130] 被围绕的区域
+ */
+
+// @lc code=start
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        if (m == 0) {
+            return;
+        }
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    dfs(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                } else {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '#' || board[i][j] == 'X') {
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+}
+// @lc code=end
