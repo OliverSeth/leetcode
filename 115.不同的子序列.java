9adbf6c9295e3d930a042401f5051e0845c257ee
OliverSeth <szl980731@126.com>
2020-03-03 15:26:07 +0800

    115

---
+++ 115.不同的子序列.java
@@ -0,0 +1,33 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2020-03-03 15:13:02 
+ * @Last Modified by: Oliver Seth
+ * @Last Modified time: 2020-03-03 15:25:35
+ */
+/*
+ * @lc app=leetcode.cn id=115 lang=java
+ *
+ * [115] 不同的子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[t.length() + 1][s.length() + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0) {
+                    dp[i][j] = 1;
+                } else if (j == 0) {
+                    dp[i][j] = 0;
+                } else if (t.charAt(i - 1) == s.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
+// @lc code=end
