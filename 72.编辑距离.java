00968a0076ccaf981b96dfa585fad27bb5852b5c
OliverSeth <szl980731@126.com>
2020-03-20 16:53:04 +0800

    72

---
+++ 72.编辑距离.java
@@ -0,0 +1,35 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2020-03-20 16:46:47 
+ * @Last Modified by: Oliver Seth
+ * @Last Modified time: 2020-03-20 16:52:20
+ */
+/*
+ * @lc app=leetcode.cn id=72 lang=java
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            for (int j = 0; j <= word2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                        dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1] - 1);
+                    } else {
+                        dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
+// @lc code=end
