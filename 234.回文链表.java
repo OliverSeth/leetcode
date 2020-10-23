2a39214a5bc08a072b6c5ed5b99971f74960340b
OliverSeth <szl980731@126.com>
2020-10-23 10:37:41 +0800

    234

---
+++ 234.回文链表.java
@@ -0,0 +1,43 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2020-10-23 10:36:41 
+ * @Last Modified by:   Oliver Seth 
+ * @Last Modified time: 2020-10-23 10:36:41 
+ */
+/*
+ * @lc app=leetcode.cn id=234 lang=java
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode p = head;
+        while (p != null) {
+            list.add(p.val);
+            p = p.next;
+        }
+        int left = 0;
+        int right = list.size() - 1;
+        while (left < right) {
+            if (!list.get(left).equals(list.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
