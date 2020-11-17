0cf63523aaeec8235eb103ede48e727db58c6e0c
OliverSeth <szl980731@126.com>
2020-11-17 15:02:50 +0800

    406

---
+++ 406.根据身高重建队列.java
@@ -0,0 +1,30 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2020-11-16 13:51:33 
+ * @Last Modified by:   Oliver Seth 
+ * @Last Modified time: 2020-11-16 13:51:33 
+ */
+/*
+ * @lc app=leetcode.cn id=406 lang=java
+ *
+ * [406] 根据身高重建队列
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0];
+            }
+        });
+        List<int[]> list = new ArrayList<>();
+        for (int[] p : people) {
+            list.add(p[1], p);
+        }
+        return list.toArray(new int[people.length][2]);
+    }
+}
+// @lc code=end
+
