8e91c7f73d37a87dfce83e41314f4e06f050dec5
OliverSeth <szl980731@126.com>
2020-11-19 11:47:17 +0800

    1030

---
+++ 1030.距离顺序排列矩阵单元格.java
@@ -0,0 +1,33 @@
+/*
+ * @Author: Oliver Seth 
+ * @Date: 2020-11-17 14:44:58 
+ * @Last Modified by:   Oliver Seth 
+ * @Last Modified time: 2020-11-17 14:44:58 
+ */
+/*
+ * @lc app=leetcode.cn id=1030 lang=java
+ *
+ * [1030] 距离顺序排列矩阵单元格
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] res = new int[R * C][2];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                res[i * C + j][0] = i;
+                res[i * C + j][1] = j;
+            }
+        }
+        Arrays.sort(res, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Math.abs(o1[0] - r0) + Math.abs(o1[1] - c0) - Math.abs(o2[0] - r0) - Math.abs(o2[1] - c0);
+            }
+        });
+        return res;
+    }
+}
+// @lc code=end
+
