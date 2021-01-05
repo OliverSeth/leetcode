380cd138d8caf450092a43d209f39f48f70ecb88
OliverSeth <szl980731@126.com>
2021-01-05 10:34:15 +0800

    509

---
+++ 509.斐波那契数.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=java
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+class Solution {
+    public int fib(int n) {
+        int[] arr = new int[Math.max(2, n + 1)];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        return arr[n];
+    }
+}
+// @lc code=end
+
