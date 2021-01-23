be6aa9d03d2d115deb7e4e2252b638ba32d8b31f
OliverSeth <szl980731@126.com>
2021-01-23 18:06:44 +0800

    628

---
+++ 628.三个数的最大乘积.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=628 lang=java
+ *
+ * [628] 三个数的最大乘积
+ */
+
+// @lc code=start
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length - 3] * nums[nums.length - 2] * nums[nums.length - 1]);
+    }
+}
+// @lc code=end
+
